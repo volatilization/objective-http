21ddd3a720ac597cab168636f4042ab72b1c4515
volatilization <volatilization@yandex.ru>
2024-03-27 18:46:42 +0300

    Created Endpoint that apply only requests with json body

--- dev/code/index.js
+++ dev/code/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     HttpServer: require('./domain/HttpServer'),
-    HttpEndpoint: require('./domain/endpoint/HttpEndpoint')
+    HttpEndpoint: require('./domain/endpoint/HttpEndpoint'),
+    JsonHttpEndpoint: require('./domain/endpoint/JsonHttpEndpoint')
 }
