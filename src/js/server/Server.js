e0b9f68ae6ee3481e5279482a8f42c71631e0fff
volatilization <volatilization@yandex.ru>
2026-03-22 03:39:59 +0300

    handler added

--- src/js/server/Server.js
+++ src/js/server/Server.js
@@ -1,28 +1,24 @@
 module.exports = class Server {
-    #endpoints;
+    #handler;
     #options;
-    #request;
-    #response;
     #http;
     #server;
 
-    constructor({ endpoints, options, request, response, http, server }) {
-        this.#endpoints = endpoints;
+    constructor({ handler, options, http, server }) {
+        this.#handler = handler;
         this.#options = options;
-        this.#request = request;
-        this.#response = response;
         this.#http = http;
         this.#server = server;
     }
 
     with({
-        endpoints = this.#endpoints,
+        handler = this.#handler,
         options = this.#options,
         http = this.#http,
         server = this.#server,
     }) {
         return new Server({
-            endpoints,
+            handler,
             options,
             http,
             server,
@@ -34,47 +30,13 @@ module.exports = class Server {
     }
 
     start() {
-        const server = this.#http.createServer(
-            async (requestStream, responseStream) => {
-                try {
-                    return await this.#response
-                        .with({
-                            response: await this.#endpoints.handle(
-                                await this.#request.copy(requestStream).flush(),
-                            ),
-                        })
-                        .flush();
-                } catch (e) {
-                    if (e.cause === 'INVALID_REQUEST') {
-                        return this.#response
-                            .copy(responseStream, {
-                                statusCode: 400,
-                                body: e.message,
-                            })
-                            .flush();
-                    }
-
-                    if (e.cause === 'HANDLER_NOT_FOUND') {
-                        return this.#response
-                            .copy(responseStream, {
-                                statusCode: 501,
-                                body: e.message,
-                            })
-                            .flush();
-                    }
-
-                    return this.#response
-                        .copy(responseStream, {
-                            statusCode: 500,
-                            body: 'Unexpected server error.',
-                        })
-                        .flush();
-                }
-            },
-        );
-
-        return new Promise((resolve) => {
-            server.listen(this.options, () => resolve(this.with({ server })));
+        return new Promise((resolve, reject) => {
+            try {
+                const server = this.#http.createServer(this.#handler.handle);
+                server.listen(this.options, () => resolve(this.with({ server })));
+            } catch (e) {
+                reject(new Error('Init server fail', {cause: {error: e, code: 'INITIAL_SERVER_FAIL'}}))
+            }
         });
     }
 
--- src/js/server/handler/index.js
+++ src/js/server/handler/index.js
@@ -1,28 +1,99 @@
-module.exports = class Handler {
+class EndpointHandler {
     #endpoint;
     #requset;
     #response;
 
-    constructor({ requset, response }) {
+    constructor({endpoint, requset, response}) {
+        this.#endpoint = endpoint;
         this.#requset = requset;
         this.#response = response;
     }
 
-    with({ requset = this.#requset, response = this.#response }) {
-        return new Handler({ requset, response });
-    }
+    async handle(requsetStream, responseStream) {
+        const requset = await this.#requset.with({
+            requsetStream
+        }).accept();
 
-    handle(requserStream, responseStream) {
+        if (JSON.stringify(requset.route) !== JSON.stringify(this.#endpoint.route)) {
+            return;
+        }
+
         return this.#response.with({
             responseStream,
-            oprtions: this.#endpoint
-                .with({
-                    route: this.#requset.with({ requserStream }).accept().route,
-                })
-                .handle(this.#requset.with({ requserStream }).accept()),
-        });
+            ...(await  this.#endpoint.handle(requset))
+        }).send()
     }
-};
+}
+
+class EndpointsHandler {
+    #endpoints;
+    #routeToEndpointMap;
+    #requset;
+    #response;
+   
+    constructor({
+        endpoints, 
+        routeToEndpointMap = new Map(endpoints.map((endpoint) => [JSON.stringify(endpoint.route), endpoint ])), 
+        requset, 
+        response
+    }) {
+        this.#endpoints = endpoints;
+        this.#routeToEndpointMap = routeToEndpointMap;
+        this.#requset = requset;
+        this.#response = response;
+    }
+
+    async handle(requsetStream, responseStream) {
+        const requset = await this.#requset.with({
+            requsetStream
+        }).accept();
+
+        if (!this.#routeToEndpointMap.has(JSON.stringify(requset.route))) {
+            throw new Error(`Handler for ${requset.route} not found`, { cause: {code: 'HANDLER_NOT_FOUND'} })
+        }
+
+        return this.#response.with({
+            responseStream,
+            ...(await  this.#routeToEndpointMap.get(JSON.stringify(requset.route)).handle(requset))
+        }).send()
+    }
+}
+
+class EndpointHandlers {
+    #handlers;
+
+    constructor({ handlers }) {
+        this.#handlers = handlers;
+    }
+
+    async handle(requsetStream, responseStream) {
+        return this.#handlers.reduce(async (response, handler) => {
+            if (response != null) {
+                return response;
+            }
+
+            await handler.handle(requsetStream, responseStream)
+        }, undefined);
+    }
+}
+
+class EndpontRequiredHandler {
+    #origin;
+
+    constructor({origin}) {
+        this.#origin = origin;
+    }
+
+    async handle(requsetStream, responseStream) {
+        const response = this.#origin.handle(requsetStream, responseStream);
+
+        if (response == null) {
+            throw new Error(`Handler for ${URL.parse(requsetStream).pathname} not found`, { cause: {code: 'HANDLER_NOT_FOUND'} })
+        }
+
+        return response;
+    }
+}
 
 class InvalidRequestErrorHandler {
     #origin;
@@ -45,7 +116,7 @@ class InvalidRequestErrorHandler {
                 responseStream,
                 status: 400,
                 message: e.message,
-            });
+            }).send();
         }
     }
 }
@@ -71,7 +142,7 @@ class HandlerNotFoundErrorHandler {
                 responseStream,
                 status: 501,
                 message: e.message,
-            });
+            }).send();
         }
     }
 }
@@ -93,7 +164,7 @@ class UnexpectedErrorHandler {
                 responseStream,
                 status: 500,
                 message: 'Application unexpected error',
-            });
+            }).send();
         }
     }
 }
