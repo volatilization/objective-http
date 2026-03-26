5343773ae24068b6c1143e496a0bd365e5422863
volatilization <36696256+volatilization@users.noreply.github.com>
2026-03-26 19:54:00 +0300

    Refactoring
    * little changes
    
    * change package version
    
    * change release it file name
    
    * change release it file and add release flow

---
+++ .release-it.mjs
@@ -0,0 +1,19 @@
+export default {
+    git: {
+        requireBranch: 'release/{version}',
+        commitMessage: 'release v${version}',
+    },
+    github: {
+        release: true,
+    },
+    npm: {
+        release: true,
+    },
+    hooks: {
+        'before:init': [
+            'git pull',
+            'npm run lint',
+            'COVERAGE_MIN_PERCENT=75 npm run test:coverage',
+        ],
+    },
+};
