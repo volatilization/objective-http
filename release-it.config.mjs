a3323e8ce26ea900034c8fc2e691bfc32d738250
volatilization <volatilization@yandex.ru>
2026-03-27 04:35:12 +0300

    create rc

---
+++ release-it.config.mjs
@@ -0,0 +1,34 @@
+import fs from 'node:fs';
+import dotenv from '@dotenvx/dotenvx';
+dotenv.config({
+    path: [
+        fs.existsSync('.env') ? '.env' : null,
+        fs.existsSync('common.env') ? 'common.env' : null,
+    ].filter((file) => file),
+});
+
+export default {
+    git: {
+        requireBranch: ['master', 'release/*'],
+        commitMessage: 'release v${version}',
+        requireCleanWorkingDir: false,
+        requireUpstream: false,
+    },
+    github: {
+        release: true,
+    },
+    npm: {
+        publish: true,
+        skipChecks: true,
+        publishPath: 'objective-http',
+    },
+    hooks: {
+        'before:init': [
+            'npm run dist:cleanup',
+            'npm run lint',
+            'npm run test:coverage',
+        ],
+        'before:npm:init': ['npm run dist:build'],
+        'after:npm:release': ['npm run dist:cleanup'],
+    },
+};
