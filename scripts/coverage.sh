c3dcf3ec747887bb1a9a2f312d90029788bc20c5
volatilization <volatilization@yandex.ru>
2026-08-28 19:35:04 +0300

    change coverage script language

---
+++ scripts/coverage.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+export LC_ALL=C
+set -euo pipefail
+
+[ -f .env ] && set -a && source .env && set +a
+[ -f common.env ] && set -a && source common.env && set +a
+
+test_result=$(FORCE_COLOR=0 npm run test)
+
+echo "$test_result"
+
+coverage_percent=$(
+    echo "$test_result" \
+    | sed -n '/start of coverage report/,/end of coverage report/p' \
+    | grep '\.js' \
+    | grep -v '/test/' \
+    | grep -v 'index.js' \
+    | awk -F'|' '{gsub(/ /, "", $2); s+=$2} END {if (s > 0) printf "%.2f", s/NR; else printf "%.2f", 0}'
+)
+
+if (( $(echo "$coverage_percent < $COVERAGE_MIN_PERCENT" | bc -l) )); then
+    echo "why am here"
+    echo "${coverage_percent} is not enough coverage percent" >&2
+    exit 1
+fi
+
+echo "${coverage_percent} is enough coverage percent"
+exit 0
